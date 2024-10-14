3a401b5e878228b1a9a7a2093f082c8504838f90
Fabio <fabioresnik@dci-student.org>
2024-10-14 15:08:12 +0200

    contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+//import { AppRouter } from "./utils/AppRouter.jsx";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    {/* <RouterProvider router={AppRouter} /> */}
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
